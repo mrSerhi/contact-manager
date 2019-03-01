16f2ee81ac60822662462b1b052ad1cd9690eb23
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-03-01 23:24:26 +0200

    Updates

---
+++ precache-manifest.3ed746b97b227d7155b14f8c69ee1440.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "85e3bf223889503391a1",
+    "url": "/contact-manager/static/js/runtime~main.85e3bf22.js"
+  },
+  {
+    "revision": "085cf832056b439449aa",
+    "url": "/contact-manager/static/js/main.085cf832.chunk.js"
+  },
+  {
+    "revision": "d752969fabc8047f1589",
+    "url": "/contact-manager/static/js/2.d752969f.chunk.js"
+  },
+  {
+    "revision": "085cf832056b439449aa",
+    "url": "/contact-manager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "d752969fabc8047f1589",
+    "url": "/contact-manager/static/css/2.85312d4e.chunk.css"
+  },
+  {
+    "revision": "9c0e1286a91f00303ceacfa419a8d203",
+    "url": "/contact-manager/index.html"
+  }
+];
